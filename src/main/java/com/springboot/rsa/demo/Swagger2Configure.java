d26c1b57f6b3b5ee7467ff2239ca8e5511ac424d
tj <houatj@163.com>
2019-05-06 09:52:06 +0800

    tijiao

--- src/main/java/com/springboot/rsa/demo/Swagger2Configure.java
+++ src/main/java/com/springboot/rsa/demo/Swagger2Configure.java
@@ -22,7 +22,7 @@ public class Swagger2Configure {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.com.springboot.rsa.demo"))
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.rsa.demo"))
                 .paths(PathSelectors.any())
                 .build();
     }
